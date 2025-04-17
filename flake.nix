7f004f30efd017d88499f383569189062c0df8dc
mikeborodin <mikeborodin@users.noreply.github.com>
2025-04-17 10:18:23 +0200

    update flake

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Flutter Version Manager - Nix flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: 
+    let
+      pkgs = import nixpkgs { inherit system; };
+      dart = pkgs.dart;
+    in
+    rec {
+      packages = {
+        default = pkgs.stdenv.mkDerivation {
+          pname = "fvm";
+          version = "0.1.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "leoafarias";
+            repo = "fvm";
+            rev = "main";
+            sha256 = null;
+          };
+
+          buildInputs = [ dart ];
+
+          buildPhase = ''
+            export PUB_CACHE=$TMPDIR/.pub-cache
+            dart pub get
+            export PATH="$PATH:/usr/bin"
+            dart compile exe bin/main.dart -o fvm 
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp fvm $out/bin/
+          '';
+
+          meta = with pkgs.lib; {
+            description = "FVM CLI application";
+            license = licenses.mit;
+          };
+        };
+      };
+    }
+  );
+}
